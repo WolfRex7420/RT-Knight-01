003fd27ade958389a4185348d1468c48ffd4442f
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-20 13:26:59 +0100

    boss

--- Assets/ScriptsFinal/BossScript.cs
+++ Assets/ScriptsFinal/BossScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+   /* public float speed;
+    
+    public int damage;
+
+    public SpriteRenderer graphics;
+    private Transform target;
+    private int destPoint = 0;
+
+    private float range;
+
+    private float minDistance = 5f;
+    private bool targetCollision = false;
+
+    public Rigidbody2D rb;
+    public CapsuleCollider2D playerCollider;
+
+    //private float thrust = 1.5f;
+    public int health;
+
+    public Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        turnedLeft = false;
+
+        if (horizontal > 0)
+        {
+            GetComponent<Animator>().Play("RunR");
+        }
+        else if (horizontal < 0)
+        {
+            GetComponent<Animator>().Play("RunL");
+            turnedLeft = true;
+        }
+        else if (horizontal == 0)
+        {
+            GetComponent<Animator>().Play("Idle");
+        }
+    }*/
 }
