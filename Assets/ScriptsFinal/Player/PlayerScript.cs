8f288da9500ea0d402bd04766bce0a0f46c3bb84
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-25 17:51:38 +0100

    Update PlayerScript.cs

--- Assets/ScriptsFinal/Player/PlayerScript.cs
+++ Assets/ScriptsFinal/Player/PlayerScript.cs
@@ -64,9 +64,8 @@ public class PlayerScript : MonoBehaviour
             Vector3 flipped = transform.localScale;
             flipped.z *= -1f;
             transform.localScale = flipped;
-            transform.Rotate(0f, 180f, 0f);
+            //transform.Rotate(0f, 180f, 0f);
             animator.SetTrigger("Dodge");
-            turnedLeft=true;
         }
         animator.ResetTrigger("Dodge");
     }
