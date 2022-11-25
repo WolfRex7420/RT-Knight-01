427f7361878edf578b9a8ea1b17fc04da95d6ef3
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-25 16:32:48 +0100

    [fix] boss

--- Assets/ScriptsFinal/Boss/Boss.cs
+++ Assets/ScriptsFinal/Boss/Boss.cs
@@ -8,7 +8,11 @@ public class Boss : MonoBehaviour
     public Transform player;
 
     public bool isFlipped = false;
-    
+
+    private bool targetCollision = false;
+
+    public int damageOnCollision;
+
 
     public void LookAtPlayer()
     {
@@ -27,5 +31,36 @@ public class Boss : MonoBehaviour
             transform.Rotate(0f, 180f, 0f);
             isFlipped = true;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !targetCollision)
+        {
+            PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damageOnCollision);
+
+            Vector3 contactPoint = collision.contacts[0].point;
+            Vector3 center = collision.collider.bounds.center;
+
+            targetCollision = true;
+
+            bool right = contactPoint.x > center.x;
+            bool left = contactPoint.x < center.x;
+            bool top = contactPoint.y > center.y;
+            bool bottom = contactPoint.y < center.y;
+
+            if (right) GetComponent<Rigidbody2D>().AddForce(transform.right, ForceMode2D.Impulse);
+            if (left) GetComponent<Rigidbody2D>().AddForce(-transform.right, ForceMode2D.Impulse);
+            if (top) GetComponent<Rigidbody2D>().AddForce(transform.up, ForceMode2D.Impulse);
+            if (bottom) GetComponent<Rigidbody2D>().AddForce(-transform.up, ForceMode2D.Impulse);
+            Invoke("FalseCollision", 0.35f);
+        }
+    }
+
+    void FalseCollision()
+    {
+        targetCollision = false;
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
     }
 }
