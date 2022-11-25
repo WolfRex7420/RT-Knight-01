d82bba6f86931b2945db9b4fe2b677298e77397f
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-11-25 13:47:06 +0100

    Update DontDestroyOnSceneLoad.cs

--- Assets/ScriptsFinal/DontDestroyOnSceneLoad.cs
+++ Assets/ScriptsFinal/DontDestroyOnSceneLoad.cs
@@ -5,19 +5,30 @@ using UnityEngine.SceneManagement;
 public class DontDestroyOnSceneLoad : MonoBehaviour
 {
     public GameObject[] objects;
+    public static DontDestroyOnSceneLoad Instance;
 
     void Awake()
     {
+        if (Instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de DontDestroyOnSceneLoad dans la scène");
+            return;
+        }
+
+        Instance = this;
         foreach (var element in objects)
         {
             DontDestroyOnLoad(element);
         }
     }
 
-    /*public RemoveFromDestroy()
+    public void RemoveFromDestroy()
     {
         //if retry button is pressed stop dont destroy on load
         //if main menu is accesed stop dont destroy on load
-        
-    }*/
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DestroyImmediate(Instance);
+        }
+    }
 }
