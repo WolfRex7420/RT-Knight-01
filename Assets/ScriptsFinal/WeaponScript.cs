589b92a7da2e2a808468bf316c35ee283983d199
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-02 02:06:24 +0100

    woohoo

---
+++ Assets/ScriptsFinal/WeaponScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{ //aha
+    public BoxCollider2D collider;
+
+    public void start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //collision.gameObject.GetComponent<EnemyPatrol>().TakeDamage(weaponDamage);
+        }
+    }
+}
