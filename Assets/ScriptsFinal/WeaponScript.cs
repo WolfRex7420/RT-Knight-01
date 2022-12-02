88ec5b0e251a988237f7aa151d111d6312daf493
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-02 16:22:12 +0100

    I'm going crazy

--- Assets/ScriptsFinal/WeaponScript.cs
+++ Assets/ScriptsFinal/WeaponScript.cs
@@ -14,34 +14,9 @@ public class WeaponScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy1"))
+        if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponent<Mob1>().TakeDamage1(weaponDamage);
-        }
-
-        if (collision.gameObject.CompareTag("Enemy2"))
-        {
-            collision.gameObject.GetComponent<Mob2>().TakeDamage2(weaponDamage);
-        }
-
-        if (collision.gameObject.CompareTag("Enemy3"))
-        {
-            collision.gameObject.GetComponent<Mob3>().TakeDamage3(weaponDamage);
-        }
-
-        if (collision.gameObject.CompareTag("Enemy4"))
-        {
-            collision.gameObject.GetComponent<Mob4>().TakeDamage4(weaponDamage);
-        }
-
-        if (collision.gameObject.CompareTag("Enemy5"))
-        {
-            collision.gameObject.GetComponent<Mob5>().TakeDamage5(weaponDamage);
-        }
-
-        if (collision.gameObject.CompareTag("Enemy6"))
-        {
-            collision.gameObject.GetComponent<Mob6>().TakeDamage6(weaponDamage);
         }
     }
 }
